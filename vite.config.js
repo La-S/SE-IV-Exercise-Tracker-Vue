e742d6d7205f8f243773d73311a17b2b087389f1
Lance Skinner <sirlancelot2604@outlook.com>
2025-10-08 09:27:34 -0500

    fix: update vite config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ dns.setDefaultResultOrder("verbatim");
 
 export default () => {
   const baseURL =
-    process.env.APP_ENV === "development" ? "/" : "/tutorial-frontend-vue3/";
+    process.env.APP_ENV === "development" ? "/" : "/seiv2025/p3/t3/";
 
   return defineConfig({
     plugins: [vue(), vuetify({ autoImport: true })],
