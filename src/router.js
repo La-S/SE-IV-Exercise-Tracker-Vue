628b5529abae6b255cac0732e5e5438677eb1394
Emily Forster <emily.forster@eagles.oc.edu>
2025-11-16 16:29:03 -0600

    Fix: removed option for individual vs. team workouts

--- src/router.js
+++ src/router.js
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHistory, createRouter } from "vue-router";
 
 import Login from "./views/Login.vue";
 
@@ -12,7 +12,7 @@ import AthleteLayout from "./layouts/AthleteLayout.vue";
 
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
