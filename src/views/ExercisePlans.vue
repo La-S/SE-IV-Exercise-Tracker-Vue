9dc68f4ee5c1b03800774a4a910675a15f548c50
GusMCordero <gus.cordero@eagles.oc.edu>
2025-11-20 13:00:01 -0600

    dashboard charts, views and summaries. also added function to exercise plans for use in dashboard

--- src/views/ExercisePlans.vue
+++ src/views/ExercisePlans.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { computed, onMounted, reactive, ref, watch } from "vue";
+import { computed, nextTick, onMounted, reactive, ref, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
 import apiClient from "../services/services.js";
 import Utils from "../config/utils.js";
 
@@ -40,6 +41,8 @@ const selectedExerciseIds = ref([]);
 const showInlineExerciseForm = ref(false);
 const exerciseSearch = ref("");
 const exerciseFocusFilter = ref("all");
+const route = useRoute();
+const router = useRouter();
 
 const newPlanDialog = ref(false);
 const editPlanDialog = ref(false);
@@ -228,6 +231,21 @@ watch(
   }
 );
 
+const shouldOpenNewPlanDialog = (value) =>
+  value === "true" || value === "1" || value === true;
+
+watch(
+  () => route.query.newPlan,
+  async (value) => {
+    if (shouldOpenNewPlanDialog(value) && !newPlanDialog.value) {
+      await nextTick();
+      newPlanDialog.value = true;
+      clearNewPlanQueryFlag();
+    }
+  },
+  { immediate: true }
+);
+
 const setTemplateLookup = () => {
   templateLookup = new Map(availableExercises.value.map((exercise) => [exercise.id, exercise]));
 };
@@ -381,6 +399,12 @@ const loadPlans = async () => {
     plansLoading.value = false;
     exercisesLoading.value = false;
   }
+};
+
+const clearNewPlanQueryFlag = () => {
+  const nextQuery = { ...route.query };
+  delete nextQuery.newPlan;
+  router.replace({ query: nextQuery });
 };
 
 const resetInlineExercise = () => {
