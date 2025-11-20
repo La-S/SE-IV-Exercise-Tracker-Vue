d9ece8c81fae9b6b45affd56fabced4ea0d9efd8
JohnEveryOC <j.every@eagles.oc.edu>
2025-11-19 20:47:00 -0600

    remove navigable buttons header

--- src/components/AdminMenu.vue
+++ src/components/AdminMenu.vue
@@ -18,8 +18,8 @@ const logoURL = ref("");
 const menuOpen = ref(false);
 
 const navItems = [
-  { label: "Users", name: "userList" },
-  { label: "Teams", name: "teamList" },
+  // { label: "Users", name: "userList" },
+  // { label: "Teams", name: "teamList" },
 ];
 
 const resetMenu = () => {
