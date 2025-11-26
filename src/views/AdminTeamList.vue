eeb40684904b6a40956edfaca8f864631c995b39
JohnEveryOC <j.every@eagles.oc.edu>
2025-11-26 17:12:14 -0600

    Add Lance's search code

--- src/views/AdminTeamList.vue
+++ src/views/AdminTeamList.vue
@@ -15,6 +15,8 @@ const addAthletesError = ref(null);
 
 const router = useRouter();
 
+const athleteSearch = ref("");
+
 const teamSections = computed(() => [
   { label: "Teams", type: "team", teams: teams.value },,
 ]);
@@ -258,6 +260,26 @@ const email = (email) => {
 const viewAthleteInfo = (athlete) => {
    router.push({ path: `athlete-info/${athlete.id}`,  });
 }
+
+const sortedAthletes = computed(() => selectedTeam.value.athletes.sort((a, b) => {return a.lastName > b.lastName}));
+const searchableAthletes = computed(() => {
+  const term = athleteSearch.value.trim().toLowerCase();
+  return availableAthletes.value.filter((athlete) => {
+    if (selectedTeam.value.athletes.find((a) => {return a == athlete})) {
+      return false;
+    }
+    
+    if (!term) {
+       return true;
+    }
+    let athleteSearchable = athlete.firstName + " " + athlete.lastName + " " + athlete.email
+    athleteSearchable = athleteSearchable.toLowerCase();
+    if (athleteSearchable.indexOf(term) != -1) {
+      return true;
+    }
+  }).sort((a, b) => {return a.lastName > b.lastName});
+});
+
 </script>
 
 <template>
@@ -284,9 +306,9 @@ const viewAthleteInfo = (athlete) => {
           <v-card-text class="flex-grow-1 overflow-y-auto pr-2">
             <v-list density="compact" nav>
                 <span v-if="teams.length > 0">
-                  <v-subheader class="text-uppercase font-weight-medium">
+                  <div class="text-uppercase font-weight-medium">                    
                     Teams
-                  </v-subheader>
+                  </div>
                   <v-alert v-if="teams.length === 0" variant="tonal" type="info">
                     There are not any teams. Use the + button to create one.
                   </v-alert>
@@ -365,7 +387,7 @@ const viewAthleteInfo = (athlete) => {
 
                 <v-expansion-panels v-else>
                   <v-expansion-panel
-                    v-for="athlete in selectedTeam.athletes ?? []"
+                    v-for="athlete in sortedAthletes ?? []"
                     :key="athlete.id"
                   >
                     <v-expansion-panel-title>
@@ -419,14 +441,22 @@ const viewAthleteInfo = (athlete) => {
           <span>Select Athletes to add to '{{ selectedTeam.name }}'</span>
         </v-card-title>
         <v-card-text>
+            <v-row class="mb-3" dense>
+            <v-text-field
+                v-model="athleteSearch"
+                label="Search athletes"
+                prepend-inner-icon="mdi-magnify"
+                density="comfortable"
+            />
+          </v-row>
           <v-list
-            v-if="selectedTeam.athletes.length < availableAthletes.length"
+            v-if="searchableAthletes.length > 0"
             density="comfortable"
             lines="two"
             style="max-height: 360px; overflow-y: auto;"
           >
             <v-item-group v-model="selectedAthleteIds" multiple>
-              <template v-for="athlete in availableAthletes" :key="athlete.id">
+              <template v-for="athlete in searchableAthletes" :key="athlete.id">
                 <v-item :value="athlete.id" v-if="!isAthleteOnTeam(athlete.id)" v-slot="{ isSelected, toggle }">
                   <v-list-item @click="toggle" class="rounded-lg">
                     <template #prepend>
@@ -444,7 +474,7 @@ const viewAthleteInfo = (athlete) => {
             </v-item-group>
           </v-list>
           <v-alert v-else type="info" variant="tonal">
-            No more athletes are available. Tell your athlete to create an account.
+            No more athletes are available. Tell your athlete to create an account or change your search term.
           </v-alert>
           <v-alert v-if="addAthletesError" type="error" variant="tonal">
            {{addAthletesError}}
