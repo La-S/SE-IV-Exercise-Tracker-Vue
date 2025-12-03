37e1a12ba40e7feb23526fe8593e4d0cbabe0df0
Lance Skinner <sirlancelot2604@outlook.com>
2025-12-03 16:31:54 -0600

    fix spacing

--- src/views/AthleteHomePage.vue
+++ src/views/AthleteHomePage.vue
@@ -55,21 +55,21 @@
             Lifting History
           </v-card-title>
           <v-card-text class="pt-2">
-  <div
-    v-if="Object.keys(personalRecords).length > 0"
-    v-for="(weight, exercise) in personalRecords"
-    :key="exercise"
-    class="d-flex justify-space-between text-body-2 mb-1"
-  >
-    <span>{{ exercise }}</span>
-    <span class="font-weight-medium">{{ weight }} lbs</span>
-  </div>
-  <div class="d-flex flex-column align-center text-center mt-2">
-    <div class="text-caption grey--text">
-      {{ liftingSubtext }}
-    </div>
-  </div>
-</v-card-text>
+            <div
+              v-if="Object.keys(personalRecords).length > 0"
+              v-for="(weight, exercise) in personalRecords"
+              :key="exercise"
+              class="d-flex justify-space-between text-body-2 mb-1"
+            >
+              <span>{{ exercise }}</span>
+              <span class="font-weight-medium">{{ weight }} lbs</span>
+            </div>
+            <div class="d-flex flex-column align-center text-center mt-2">
+              <div class="text-caption grey--text">
+                {{ liftingSubtext }}
+              </div>
+            </div>
+          </v-card-text>
         </v-card>
       </v-col>
     </v-row>
