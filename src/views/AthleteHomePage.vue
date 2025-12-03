e0a7f1b2eeec8981395a4757d4e90a11e0970c97
Lance Skinner <sirlancelot2604@outlook.com>
2025-12-03 17:07:08 -0600

    feat: componentize athlete lifting history card

--- src/views/AthleteHomePage.vue
+++ src/views/AthleteHomePage.vue
@@ -50,27 +50,10 @@
 
     <v-row dense>
       <v-col cols="12">
-        <v-card class="pa-3 elevation-2" rounded="xl">
-          <v-card-title class="text-subtitle-1 font-weight-bold">
-            Lifting History
-          </v-card-title>
-          <v-card-text class="pt-2">
-            <div
-              v-if="Object.keys(personalRecords).length > 0"
-              v-for="(weight, exercise) in personalRecords"
-              :key="exercise"
-              class="d-flex justify-space-between text-body-2 mb-1"
-            >
-              <span>{{ exercise }}</span>
-              <span class="font-weight-medium">{{ weight }} lbs</span>
-            </div>
-            <div class="d-flex flex-column align-center text-center mt-2">
-              <div class="text-caption grey--text">
-                {{ liftingSubtext }}
-              </div>
-            </div>
-          </v-card-text>
-        </v-card>
+        <athlete-lifting-history-card
+          :personalRecords="personalRecords"
+          :liftingSubtext="liftingSubtext"
+        />
       </v-col>
     </v-row>
   </v-container>
@@ -83,6 +66,7 @@ import Utils from "../config/utils.js";
 import dayjs from 'dayjs';
 import WorkoutCompletionCard from "../components/WorkoutCompletionCard.vue";
 import AverageMileTimeCard from "../components/AverageMileTimeCard.vue";
+import AthleteLiftingHistoryCard from "../components/AthleteLiftingHistoryCard.vue";
 
 const completedThisWeek = ref(0);
 const totalThisWeek = ref(0);
